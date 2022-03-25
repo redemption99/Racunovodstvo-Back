95076dd6b4f08d7849ae727aa9be14878e777a1a
dvasiljevic9118rn <101745890+dvasiljevic9118rn@users.noreply.github.com>
2022-03-25 17:06:27 +0100

    Specification Generalizacija (#40)

--- src/main/java/rs/raf/demo/controllers/FakturaRestController.java
+++ src/main/java/rs/raf/demo/controllers/FakturaRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import rs.raf.demo.model.Faktura;
 import rs.raf.demo.services.IFakturaService;
 import rs.raf.demo.services.impl.FakturaService;
-import rs.raf.demo.specifications.FakturaSpecificationsBuilder;
+import rs.raf.demo.specifications.RacunSpecificationsBuilder;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -68,7 +68,7 @@ public class FakturaRestController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> search(@RequestParam(name = "search") String search){
-        FakturaSpecificationsBuilder builder = new FakturaSpecificationsBuilder();
+        RacunSpecificationsBuilder<Faktura> builder = new RacunSpecificationsBuilder<>();
         Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
         Matcher matcher = pattern.matcher(search + ",");
         while (matcher.find()) {
