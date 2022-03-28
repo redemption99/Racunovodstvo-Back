048e783792c87d890fd1c808f2b9804a1e7698d0
redemption99 <fcosovic1018rn@raf.rs>
2022-03-28 19:10:40 +0200

    Suma duguje, suma potrazuje, saldo

--- src/main/java/rs/raf/demo/services/impl/DokumentService.java
+++ src/main/java/rs/raf/demo/services/impl/DokumentService.java
@@ -1,31 +1,76 @@
 package rs.raf.demo.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rs.raf.demo.model.Dokument;
-import rs.raf.demo.services.IService;
+import rs.raf.demo.model.Knjizenje;
+import rs.raf.demo.model.Konto;
+import rs.raf.demo.repositories.DokumentRepository;
+import rs.raf.demo.services.IDokumentService;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class DokumentService implements IService<Dokument, Long> {
-    @Override
-    public <S extends Dokument> S save(S var1) {
-        return null;
+public class DokumentService implements IDokumentService {
+
+    private final DokumentRepository dokumentRepository;
+
+    @Autowired
+    public DokumentService(DokumentRepository dokumentRepository) {
+        this.dokumentRepository = dokumentRepository;
     }
 
     @Override
-    public Optional<Dokument> findById(Long var1) {
-        return Optional.empty();
+    public <S extends Dokument> S save(S var1) {
+        return dokumentRepository.save(var1);
+    }
+
+    @Override
+    public Optional<Dokument> findById(Long id) {
+        return dokumentRepository.findById(id);
     }
 
     @Override
     public List<Dokument> findAll() {
-        return null;
+        return dokumentRepository.findAll();
     }
 
     @Override
     public void deleteById(Long var1) {
 
+    }
+
+    @Override
+    public Double getSumaPotrazuje(Long id) {
+        Optional<Dokument> dok = findById(id);
+        List<Knjizenje> knjizenja = dok.get().getKnjizenje();
+        Double suma = Double.valueOf(0);
+        for (Knjizenje i : knjizenja) {
+            List<Konto> allKonto = i.getKonto();
+            for (Konto j: allKonto) {
+                suma += j.getPotrazuje();
+            }
+        }
+        return suma;
+    }
+
+    @Override
+    public Double getSumaDuguje(Long id) {
+        Optional<Dokument> dok = findById(id);
+        List<Knjizenje> knjizenja = dok.get().getKnjizenje();
+        Double suma = Double.valueOf(0);
+        for (Knjizenje i : knjizenja) {
+            List<Konto> allKonto = i.getKonto();
+            for (Konto j: allKonto) {
+                suma += j.getDuguje();
+            }
+        }
+        return suma;
+    }
+
+    @Override
+    public Double getSaldo(Long id) {
+        return getSumaPotrazuje(id) - getSumaDuguje(id);
     }
 }
