cc3aa22c7ffa41f941e2a2093bfee97743b80db2
matijagojkovic <gojkovicmatija99@gmail.com>
2022-04-10 15:48:39 +0200

    Dodat swagger

--- knjizenje/src/main/java/raf/si/racunovodstvo/knjizenje/filter/AuthFilter.java
+++ knjizenje/src/main/java/raf/si/racunovodstvo/knjizenje/filter/AuthFilter.java
@@ -6,10 +6,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -20,28 +22,35 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthFilter extends OncePerRequestFilter {
 
     @Autowired
-    private RestTemplate restTemplate;
+    RestTemplate restTemplate;
 
     private static final String URL = "http://user/auth/access";
+
+    private static final String[] EXCLUDED_URLS = {"/v3/api-docs", "/swagger-ui.html", "/swagger-ui/"};
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
         throws ServletException, IOException {
         try {
-            String authHeader = httpServletRequest.getHeader("Authorization");
+            if (!skipFilter(httpServletRequest.getRequestURL().toString())) {
+                String authHeader = httpServletRequest.getHeader("Authorization");
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Authorization", authHeader);
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("Authorization", authHeader);
+                HttpEntity request = new HttpEntity(headers);
 
-            HttpEntity request = new HttpEntity(headers);
-
-            // baca izuzetak ako nije ispravak token
-            ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.GET, request, String.class);
-
+                // baca izuzetak ako nije ispravak token
+                ResponseEntity<String> response = restTemplate.exchange(URL, HttpMethod.GET, request, String.class);
+            }
             filterChain.doFilter(httpServletRequest, httpServletResponse);
-        } catch (Exception e) {
+
+        } catch (HttpClientErrorException e) {
             httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "The token is not valid.");
         }
 
+    }
+
+    private Boolean skipFilter(String url) {
+        return Arrays.stream(EXCLUDED_URLS).sequential().anyMatch(exUrl -> url.contains(exUrl));
     }
 }
