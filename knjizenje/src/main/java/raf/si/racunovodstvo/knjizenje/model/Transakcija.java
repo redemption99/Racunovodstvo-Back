a3652bf33054e66a8915679a95d1b00905790f17
boris.stojanovic <boris.stojanovic@msg-global.com>
2022-05-22 14:01:33 +0200

    feature/transakcije: Izmenio model za dokument

--- knjizenje/src/main/java/raf/si/racunovodstvo/knjizenje/model/Transakcija.java
+++ knjizenje/src/main/java/raf/si/racunovodstvo/knjizenje/model/Transakcija.java
@@ -28,6 +28,4 @@ public class Transakcija extends Dokument{
     @ManyToOne
     @JoinColumn(name = "sifra")
     private SifraTransakcije sifraTransakcije;
-    @Column
-    private Long preduzeceId;
 }
