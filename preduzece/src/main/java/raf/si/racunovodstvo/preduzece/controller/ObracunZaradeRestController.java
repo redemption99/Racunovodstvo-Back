1cd837182e0ed67219faa149ea583e53e71a6611
aleksadacic <aleksa.dacic@comtrade.com>
2022-06-27 12:21:16 +0200

    Rucno kreiranje obracuna zarade

--- preduzece/src/main/java/raf/si/racunovodstvo/preduzece/controller/ObracunZaradeRestController.java
+++ preduzece/src/main/java/raf/si/racunovodstvo/preduzece/controller/ObracunZaradeRestController.java
@@ -1,6 +1,7 @@
 package raf.si.racunovodstvo.preduzece.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,10 +11,16 @@ import raf.si.racunovodstvo.preduzece.jobs.ObracunZaradeJob;
 import raf.si.racunovodstvo.preduzece.requests.ObracunZaradeConfigRequest;
 import raf.si.racunovodstvo.preduzece.responses.ObracunZaradeConfigResponse;
 import raf.si.racunovodstvo.preduzece.responses.SifraTransakcijeResponse;
+import raf.si.racunovodstvo.preduzece.services.impl.ObracunZaposleniService;
+import raf.si.racunovodstvo.preduzece.services.impl.ObracunZaradeService;
 import raf.si.racunovodstvo.preduzece.validation.groups.OnCreate;
 
 import javax.persistence.EntityNotFoundException;
+import javax.print.attribute.standard.Media;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Produces;
 import java.time.DateTimeException;
+import java.util.Date;
 
 @CrossOrigin
 @RestController
@@ -22,10 +29,12 @@ import java.time.DateTimeException;
 public class ObracunZaradeRestController {
     private final ObracunZaradeJob obracunZaradeJob;
     private final TransakcijeFeignClient transakcijeFeignClient;
+    private final ObracunZaposleniService obracunZaposleniService;
 
-    public ObracunZaradeRestController(ObracunZaradeJob obracunZaradeJob, TransakcijeFeignClient transakcijeFeignClient) {
+    public ObracunZaradeRestController(ObracunZaradeJob obracunZaradeJob, TransakcijeFeignClient transakcijeFeignClient, ObracunZaposleniService obracunZaposleniService) {
         this.obracunZaradeJob = obracunZaradeJob;
         this.transakcijeFeignClient = transakcijeFeignClient;
+        this.obracunZaposleniService = obracunZaposleniService;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -73,6 +82,11 @@ public class ObracunZaradeRestController {
         obracunZaradeConfigResponse.setDayOfMonth(obracunZaradeJob.getDayOfMonth());
 
         return ResponseEntity.ok(obracunZaradeConfigResponse);
+    }
+
+    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createObracunZarade(@NotNull @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date datum, @NotNull @RequestParam Long idTransakcije) {
+        return ResponseEntity.ok(obracunZaposleniService.makeObracun(datum, idTransakcije));
     }
 }
 
